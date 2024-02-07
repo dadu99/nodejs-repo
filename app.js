54f220385f2095ec3d57b62769d7e39d8f2baa5f
Darius Baciu <baciu.darius01@gmail.com>
2024-02-07 14:37:15 +0200

    rendering server-side html via EJS

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Setting the view engine to EJS and views directory
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Using body-parser middleware to parse URL-encoded data
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Middleware function to set the Content-Type header
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "text/html");
   next(); //next to the middleware line
 });
 
 app.use((req, res, next) => {
+  // Extracting user name from request body or setting it to "Unknown User" if not present
   const userName = req.body.username || "Unknown User";
-  res.send(
-    `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
-  );
+  res.render("index", {
+    user: userName,
+  });
 });
 
 app.listen(8080);
